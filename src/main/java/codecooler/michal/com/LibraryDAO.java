bce4d3495a5213953d5fef2efba9bd3441ecc9aa
misohan <michal.hanusniak@aiesec.cz>
2020-04-19 13:44:13 +0200

    LibraryDAO update

--- src/main/java/codecooler/michal/com/LibraryDAO.java
+++ src/main/java/codecooler/michal/com/LibraryDAO.java
@@ -1,4 +1,9 @@
 package codecooler.michal.com;
 
 public interface LibraryDAO {
+    void editBookData(long ISBN, int author_id, String title, String publisher_id, int publication_year, int price);
+    void removeBook(int author_id);
+    void findBookByAuthorSurname(String authorSurname);
+    void showAllBooksByTitleAsc();
+
 }
