c91ecadce1559aa8cf2175b821948aa6e3186a02
misohan <michal.hanusniak@aiesec.cz>
2020-04-19 13:19:00 +0200

    Update Library

--- src/main/java/codecooler/michal/com/Library.java
+++ src/main/java/codecooler/michal/com/Library.java
@@ -2,6 +2,13 @@ package codecooler.michal.com;
 
 public class Library {
     public static void main(String[] args) {
-        
+
+        LibraryDAOImpl libraryDAO = new LibraryDAOImpl();
+
+//        libraryDAO.editBookData(97800995,6,"Hag Seed","5k4",2017,45);
+        libraryDAO.removeBook(4);
+        libraryDAO.showAllBooksByTitleAsc();
+
     }
+
 }
