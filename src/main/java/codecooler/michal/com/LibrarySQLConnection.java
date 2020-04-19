993bcb3c62cae11c27550c56b3e0572331ad62c1
misohan <michal.hanusniak@aiesec.cz>
2020-04-19 11:03:25 +0200

    Update LibrarySQLConnection

--- src/main/java/codecooler/michal/com/LibrarySQLConnection.java
+++ src/main/java/codecooler/michal/com/LibrarySQLConnection.java
@@ -1,4 +1,51 @@
 package codecooler.michal.com;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Properties;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 public class LibrarySQLConnection {
+
+    private Properties readProperties() {
+        Properties props = new Properties();
+        Path myPath = Paths.get("src/main/resources/database.properties");
+
+        try {
+            BufferedReader bf = Files.newBufferedReader(myPath,
+                    StandardCharsets.UTF_8);
+
+            props.load(bf);
+        } catch (IOException ex) {
+            Logger.getLogger(LibrarySQLConnection.class.getName()).log(
+                    Level.SEVERE, null, ex);
+        }
+
+        return props;
+    }
+
+    public Connection connect() {
+        // SQL connection
+        Properties props = readProperties();
+
+        String url = props.getProperty("db.url");
+        String user = props.getProperty("db.user");
+        String password = props.getProperty("db.password");
+
+        Connection conn = null;
+        try {
+            conn = DriverManager.getConnection(url, user, password);
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+        return conn;
+    }
 }
