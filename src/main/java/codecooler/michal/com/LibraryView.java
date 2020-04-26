2c905727c252065dc0e600d91b919e704f4326da
misohan <michal.hanusniak@aiesec.cz>
2020-04-26 15:38:32 +0200

    Update LibraryView

--- src/main/java/codecooler/michal/com/LibraryView.java
+++ src/main/java/codecooler/michal/com/LibraryView.java
@@ -5,14 +5,16 @@ import java.util.List;
 public class LibraryView {
 
     public void viewBook(Book book){
-        System.out.println("====================");
-        System.out.println("|ISBN: " + book.getISBN());
+        System.out.println("|¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯|");
+        System.out.println("|--------------------------------------|");
+        System.out.println("|ISBN: " + book.getISBN() +"                   |");
+        System.out.println("|--------------------------------------|");
         System.out.println("|Author id: " + book.getAuthor_id());
         System.out.println("|Title: " + book.getTitle());
         System.out.println("|Publisher id: " + book.getPublisher_id());
         System.out.println("|Publication year: " + book.getPublication_year());
         System.out.println("|Price: " + book.getPrice());
-        System.out.println("=====================");
+        System.out.println("----------------------------------------");
 
     }
     public void viewBooks(List<Book> books){
@@ -20,4 +22,5 @@ public class LibraryView {
             viewBook(book);
         }
     }
+
 }
