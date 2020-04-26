6711e55639dc3d5a7e8d5671cebb4552b7f69717
misohan <michal.hanusniak@aiesec.cz>
2020-04-26 20:46:07 +0200

    Update Library

--- src/main/java/codecooler/michal/com/Library.java
+++ src/main/java/codecooler/michal/com/Library.java
@@ -3,10 +3,9 @@ package codecooler.michal.com;
 public class Library {
     public static void main(String[] args) {
 
-        LibraryController libraryController = new LibraryController();
-        LibraryDAOImpl libraryDAO = new LibraryDAOImpl();
+       LibraryController libraryController = new LibraryController();
+       libraryController.libraryOptions();
 
-        libraryController.libraryOptions(libraryDAO);
 
     }
 
