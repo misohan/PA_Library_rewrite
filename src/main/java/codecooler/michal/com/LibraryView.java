b628c6c4ed185ea196ade1eb068ba01a6a2389bd
misohan <michal.hanusniak@aiesec.cz>
2020-04-26 14:43:52 +0200

    Add viewBook method to LibraryView

--- src/main/java/codecooler/michal/com/LibraryView.java
+++ src/main/java/codecooler/michal/com/LibraryView.java
@@ -1,4 +1,16 @@
 package codecooler.michal.com;
 
 public class LibraryView {
+
+    public void viewBook(Book book){
+        System.out.println("====================");
+        System.out.println("|ISBN: " + book.getISBN());
+        System.out.println("|Author id: " + book.getAuthor_id());
+        System.out.println("|Title: " + book.getTitle());
+        System.out.println("|Publisher id: " + book.getPublisher_id());
+        System.out.println("|Publication year: " + book.getPublication_year());
+        System.out.println("|Price: " + book.getPrice());
+        System.out.println("=====================");
+
+    }
 }
