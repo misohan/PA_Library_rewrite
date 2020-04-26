7f91e6e09d35cb1e9bfdad620064a52580ee26d9
misohan <michal.hanusniak@aiesec.cz>
2020-04-26 14:04:50 +0200

    LibraryDAO change names of methods and update methods type

--- src/main/java/codecooler/michal/com/LibraryDAO.java
+++ src/main/java/codecooler/michal/com/LibraryDAO.java
@@ -3,13 +3,13 @@ package codecooler.michal.com;
 import java.util.List;
 
 public interface LibraryDAO {
-    void addBookToLibrary(long ISBN, int author_id, String title, String publisher_id, int publication_year, int price);
-    void updateBookInLibrary(long ISBN, int author_id, String title, String publisher_id, int publication_year, int price);
+    void addBook(long ISBN, int author_id, String title, String publisher_id, int publication_year, int price);
+    void updateBook(long ISBN, int author_id, String title, String publisher_id, int publication_year, int price);
     void removeBook(long author_id);
-    void findBookByAuthorSurname(String surname);
-    List<Book> showAllBooksByTitleAsc();
-    void numberOfBooksByAuthor(String surname);
-    void showBooksForLastTenYears();
+    Book findBookByAuthorSurname(String surname);
+    List<Book> getAllBooksByTitleAsc();
+    void getNumberOfBooksByAuthor(String surname);
+    void getBooksForLastTenYears();
     int priceOfAllBooks();
 
 }
