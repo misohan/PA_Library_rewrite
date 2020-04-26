8b67d3eb1fb03ef07354704e4c1d50034d886bcc
misohan <michal.hanusniak@aiesec.cz>
2020-04-26 11:19:47 +0200

    Correct addBookToLibrary method in LibraryDAO

--- src/main/java/codecooler/michal/com/LibraryDAO.java
+++ src/main/java/codecooler/michal/com/LibraryDAO.java
@@ -1,7 +1,7 @@
 package codecooler.michal.com;
 
 public interface LibraryDAO {
-    void addBookToLibrary(int ISBN, int author_id, String title, String publisher_id, int publication_year, int price);
+    void addBookToLibrary(long ISBN, int author_id, String title, String publisher_id, int publication_year, int price);
     void editBookData(String title, int publication_year, int price, int author_id);
     void removeBook(long author_id);
     void findBookByAuthorSurname(String surname);
