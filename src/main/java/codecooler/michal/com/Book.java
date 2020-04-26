ae7cae42f8c50af0728ae86c5d8cc17a3fbf77c1
misohan <michal.hanusniak@aiesec.cz>
2020-04-26 12:52:20 +0200

    Add fields, getters and setters to Book

--- src/main/java/codecooler/michal/com/Book.java
+++ src/main/java/codecooler/michal/com/Book.java
@@ -1,4 +1,58 @@
 package codecooler.michal.com;
 
 public class Book {
+    private long ISBN;
+    private int author_id;
+    private String title;
+    private String publisher_id;
+    private int publication_year;
+    private int price;
+
+    public long getISBN() {
+        return ISBN;
+    }
+
+    public void setISBN(long ISBN) {
+        this.ISBN = ISBN;
+    }
+
+    public int getAuthor_id() {
+        return author_id;
+    }
+
+    public void setAuthor_id(int author_id) {
+        this.author_id = author_id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getPublisher_id() {
+        return publisher_id;
+    }
+
+    public void setPublisher_id(String publisher_id) {
+        this.publisher_id = publisher_id;
+    }
+
+    public int getPublication_year() {
+        return publication_year;
+    }
+
+    public void setPublication_year(int publication_year) {
+        this.publication_year = publication_year;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
 }
