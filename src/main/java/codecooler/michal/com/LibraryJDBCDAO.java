fec718954402406f31374e87bac7812409c95453
misohan <michal.hanusniak@aiesec.cz>
2020-04-26 10:48:20 +0200

    Update LibraryJDBCDAO

--- src/main/java/codecooler/michal/com/LibraryJDBCDAO.java
+++ src/main/java/codecooler/michal/com/LibraryJDBCDAO.java
@@ -1,22 +1,24 @@
 package codecooler.michal.com;
 
+import java.math.BigDecimal;
 import java.math.BigInteger;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import java.sql.ResultSet;
 
-public class LibraryDAOImpl {
+public class LibraryJDBCDAO implements LibraryDAO {
     private LibrarySQLConnection dbConn = new LibrarySQLConnection();
     private ResultSet resultSet = null;
 
-    public void editBookData(long ISBN, int author_id, String title, String publisher_id, int publication_year, int price){
+    public void updateBookInLibrary(long ISBN, int author_id, String title, String publisher_id, int publication_year, int price){
         String sql = "UPDATE books "
-                + "SET ISBN= ?, title= ?, publisherID= ?, publicationYear= ?, price = ? "
+                + "SET \"ISBN\" = ?, \"title\" = ?, \"publisher_id\"= ?, \"publication_year\"= ?, \"price\" = ?"
                 + "WHERE author_id = ?";
 
         try (Connection con = dbConn.connect();
@@ -25,11 +27,11 @@ public class LibraryDAOImpl {
             // set the corresponding parameter
 
             pst.setLong(1, ISBN);
-            pst.setInt(2, author_id);
-            pst.setString(3, title);
-            pst.setString(4,publisher_id);
-            pst.setInt(5,publication_year);
-            pst.setInt(6,price);
+            pst.setInt(6, author_id);
+            pst.setString(2, title);
+            pst.setString(3,publisher_id);
+            pst.setInt(4,publication_year);
+            pst.setInt(5,price);
 
 
             // update
@@ -39,15 +41,38 @@ public class LibraryDAOImpl {
             System.out.println(e.getMessage());
         }
     }
-    public void removeBook(String title){
-        String sql = "DELETE from books WHERE \"title\" = ?";
-
+    public void editBookData(String title, int publication_year, int price, int author_id){
+        String sql = "UPDATE books "
+                +"SET \"title\" = ?, \"publication_year\" = ?, \"price\" = ?"
+                + "WHERE author_id = ?";
+        System.out.println(sql);
         try (Connection con = dbConn.connect();
              PreparedStatement pst = con.prepareStatement(sql)) {
 
             // set the corresponding parameter
 
             pst.setString(1, title);
+            pst.setInt(2,publication_year);
+            pst.setInt(3,price);
+            pst.setInt(4,author_id);
+
+
+            // update
+            pst.executeUpdate();
+
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+    public void removeBook(long ISBN){
+        String sql = "DELETE from books WHERE \"ISBN\" = ?";
+
+        try (Connection con = dbConn.connect();
+             PreparedStatement pst = con.prepareStatement(sql)) {
+
+            // set the corresponding parameter
+
+            pst.setLong(1, ISBN);
 
             // update
             pst.executeUpdate();
@@ -58,20 +83,98 @@ public class LibraryDAOImpl {
 
 
     }
-    public void findBookByAuthorSurname(String authorSurname){
-        String sql = "SELECT*FROM from authors WHERE \"author_id\" = ?";
+    public void findBookByAuthorSurname(String surname){
+        String sql = "SELECT books.\"title\", authors.\"surname\" " +
+                "FROM authors " +
+                "INNER JOIN books ON books.\"author_id\"=authors.\"id\"" +
+                " WHERE \"surname\" = '" + surname + "';";
+
+        try (Connection con = dbConn.connect();
+             PreparedStatement pst = con.prepareStatement(sql)) {
+
+            resultSet = pst.executeQuery();
+
+            System.out.println("Titles by " + surname);
+            while (resultSet.next()) {
+                System.out.println("Title: " + resultSet.getString("title"));
+            }
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+    public void showAllBooksByTitleAsc(){
+
+        String sql = "SELECT * FROM books ORDER BY \"title\" ASC";
+
+        try (Connection con = dbConn.connect();
+
+                 PreparedStatement pst = con.prepareStatement(sql)) {
+
+            resultSet = pst.executeQuery();
+
+            while (resultSet.next()) {
+                System.out.println("Title: " + resultSet.getString("title"));
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+    public void numberOfBooksByAuthor(String surname){
+        String sql = "SELECT COUNT(*)" +
+                "FROM authors " +
+                "INNER JOIN books ON books.\"author_id\"=authors.\"id\"" +
+                " WHERE \"surname\" = '" + surname + "';";
+
+        try (Connection con = dbConn.connect();
+             PreparedStatement pst = con.prepareStatement(sql)) {
+
+            resultSet = pst.executeQuery();
+
+            while (resultSet.next()) {
+                System.out.println("You have " + resultSet.getInt(1) + " books by this author.");
+            }
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+    public void showBooksForLastTenYears(){
+        Calendar calendar = Calendar.getInstance();
+        int currentYear = calendar.get(Calendar.YEAR);
+        int lastTenYears= 10;
+
+        System.out.println(currentYear);
+
+        String sql = "SELECT * FROM books " +
+                "WHERE publication_year " +
+                "BETWEEN " + (currentYear - lastTenYears) +" AND " + currentYear;
+
+        try (Connection con = dbConn.connect();
+
+             PreparedStatement pst = con.prepareStatement(sql)) {
+
+            resultSet = pst.executeQuery();
+
+            while (resultSet.next()) {
+                System.out.println("Title: " + resultSet.getString("title"));
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void priceOfAllBooks(){
+        String sql = "SELECT SUM(price) " +
+                "FROM books;";
 
         try (Connection con = dbConn.connect();
              PreparedStatement pst = con.prepareStatement(sql)) {
 
 
-            pst.setString(1, authorSurname);
-
             resultSet = pst.executeQuery();
 
             if (resultSet.next()) {
 
-                System.out.println("Title: " + resultSet.getString("title"));
+                System.out.println("Value of Library: " + resultSet.getInt(1));
             }
             resultSet.close();
 
@@ -81,23 +184,12 @@ public class LibraryDAOImpl {
             System.out.println(e.getMessage());
         }
     }
-    public void showAllBooksByTitleAsc(){
+    public void addNewBookToLibrary(Long longy){
 
-        String sql = "SELECT * FROM books ORDER BY \"title\" ASC";
-
-        try
-                (Connection con = dbConn.connect();
-                 PreparedStatement pst = con.prepareStatement(sql)) {
-            resultSet = pst.executeQuery();
-            while (resultSet.next()) {
-                System.out.println("Title: " + resultSet.getString("title"));
-
-            }
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
     }
 
-
-
 }
+
+
+
+
