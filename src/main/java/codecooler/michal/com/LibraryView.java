ab352ecb0592649c07dcae842d2b10861776d88d
misohan <michal.hanusniak@aiesec.cz>
2020-04-26 14:45:23 +0200

    Add viewBooks method to LibraryView

--- src/main/java/codecooler/michal/com/LibraryView.java
+++ src/main/java/codecooler/michal/com/LibraryView.java
@@ -1,5 +1,7 @@
 package codecooler.michal.com;
 
+import java.util.List;
+
 public class LibraryView {
 
     public void viewBook(Book book){
@@ -12,5 +14,10 @@ public class LibraryView {
         System.out.println("|Price: " + book.getPrice());
         System.out.println("=====================");
 
+    }
+    public void viewBooks(List<Book> books){
+        for (Book book: books){
+            viewBook(book);
+        }
     }
 }
