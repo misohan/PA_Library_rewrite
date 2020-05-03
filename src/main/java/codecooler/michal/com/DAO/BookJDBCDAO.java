ce4734e5be1719453844ab65cbc1163a96b77200
misohan <michal.hanusniak@aiesec.cz>
2020-05-03 14:20:01 +0200

    Create methods updateBook, getBooks and removeBook

--- src/main/java/codecooler/michal/com/DAO/BookJDBCDAO.java
+++ src/main/java/codecooler/michal/com/DAO/BookJDBCDAO.java
@@ -4,11 +4,14 @@ import codecooler.michal.com.model.Book;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 
 
 public class BookJDBCDAO implements BookDAO {
+    private List<Book> books = new ArrayList<>();
     private final LibrarySQLConnection dbConn = new LibrarySQLConnection();
     private LibrarySQLConnection connection;
 
@@ -41,16 +44,75 @@ public class BookJDBCDAO implements BookDAO {
 
     @Override
     public void updateBook(Book book) {
+        String sql = "UPDATE books "
+                + "SET \"author_id\" = ?, \"title\" = ?, \"publisher_id\"= ?, \"publication_year\"= ?, \"price\" = ?"
+                + "WHERE \"ISBN\" = ?";
 
+        try (Connection con = dbConn.connect();
+             PreparedStatement pst = con.prepareStatement(sql)) {
+
+            pst.setLong(6, book.getISBN());
+            pst.setInt(1, book.getAuthor_id());
+            pst.setString(2, book.getTitle());
+            pst.setString(3, book.getPublisher_id());
+            pst.setInt(4, book.getPublication_year());
+            pst.setInt(5, book.getPrice());
+
+            pst.executeUpdate();
+
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
     }
 
     @Override
     public List<Book> getBooks() {
+        ResultSet resultSet = null;
+        List<Book> books = new ArrayList<>();
+
+        String sql = "SELECT * FROM books";
+
+        try (Connection con = dbConn.connect();
+             PreparedStatement pst = con.prepareStatement(sql)) {
+
+            resultSet = pst.executeQuery();
+
+            while (resultSet.next()) {
+
+                Book book = new Book();
+
+                book.setISBN(resultSet.getLong("ISBN"));
+                book.setAuthor_id(resultSet.getInt("author_id"));
+                book.setTitle(resultSet.getString("title"));
+                book.setPublisher_id(resultSet.getString("publisher_id"));
+                book.setPublication_year(resultSet.getInt("publication_year"));
+                book.setPrice(resultSet.getInt("price"));
+
+                books.add(book);
+            }
+            return books;
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
         return null;
     }
 
     @Override
     public void deleteBook(long ISBN) {
+        String sql = "DELETE from books WHERE \"ISBN\" = ?";
+
+        try (Connection con = dbConn.connect();
+             PreparedStatement pst = con.prepareStatement(sql)) {
+
+            pst.setLong(1, ISBN);
+
+            pst.executeUpdate();
+
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+    }
 
     }
 
