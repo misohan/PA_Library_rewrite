c482bc8d109a6e9e61d76faaa1d789bda12da4c0
misohan <michal.hanusniak@aiesec.cz>
2020-05-03 16:00:03 +0200

    Update methods

--- src/main/java/codecooler/michal/com/DAO/PublisherDAO.java
+++ src/main/java/codecooler/michal/com/DAO/PublisherDAO.java
@@ -1,4 +1,13 @@
 package codecooler.michal.com.DAO;
 
+import codecooler.michal.com.model.Book;
+import codecooler.michal.com.model.Publisher;
+
+import java.util.List;
+
 public interface PublisherDAO {
+    void createPublisher(Publisher publisher);
+    void updatePublisher(Publisher publisher);
+    List<Publisher> getPublishers();
+    void deletePublisher(String id);
 }
