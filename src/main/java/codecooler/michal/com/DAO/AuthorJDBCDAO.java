6aa0faab0c6c2add9cef211f4ee3dc498cde657d
misohan <michal.hanusniak@aiesec.cz>
2020-05-03 14:32:40 +0200

    Override methods

--- src/main/java/codecooler/michal/com/DAO/AuthorJDBCDAO.java
+++ src/main/java/codecooler/michal/com/DAO/AuthorJDBCDAO.java
@@ -1,5 +1,6 @@
 package codecooler.michal.com.DAO;
 import codecooler.michal.com.LibrarySQLConnection;
+import codecooler.michal.com.model.Author;
 import codecooler.michal.com.model.Book;
 
 import java.sql.Connection;
@@ -9,10 +10,28 @@ import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 
-public class AuthorJDBCDAO {
+public class AuthorJDBCDAO implements AuthorDAO {
     private final LibrarySQLConnection dbConn = new LibrarySQLConnection();
     private LibrarySQLConnection connection;
 
 
+    @Override
+    public void createAuthor(Author author) {
 
+    }
+
+    @Override
+    public void updateAuthor(Author author) {
+
+    }
+
+    @Override
+    public List<Author> getAuthors() {
+        return null;
+    }
+
+    @Override
+    public void deleteAuthor(int id) {
+
+    }
 }
