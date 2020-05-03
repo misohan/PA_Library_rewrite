595165e14db88db9ea54c9f4a7c16d7adedf0ef5
misohan <michal.hanusniak@aiesec.cz>
2020-05-03 14:00:23 +0200

    Create method CreateBook

--- src/main/java/codecooler/michal/com/DAO/BookJDBCDAO.java
+++ src/main/java/codecooler/michal/com/DAO/BookJDBCDAO.java
@@ -1,5 +1,57 @@
 package codecooler.michal.com.DAO;
+import codecooler.michal.com.LibrarySQLConnection;
+import codecooler.michal.com.model.Book;
 
-public class BookJDBCDAO {
-    
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.List;
+
+
+public class BookJDBCDAO implements BookDAO {
+    private final LibrarySQLConnection dbConn = new LibrarySQLConnection();
+    private LibrarySQLConnection connection;
+
+    public BookJDBCDAO() {
+        this.connection = new LibrarySQLConnection();
+    }
+
+    @Override
+    public void createBook(Book book){
+
+        String sql = "INSERT INTO books (\"ISBN\",\"author_id\", \"title\", \"publisher_id\", \"publication_year\", \"price\") " +
+                "VALUES (?,?,?,?,?,?)";
+
+        try (Connection con = dbConn.connect();
+             PreparedStatement pst = con.prepareStatement(sql)) {
+
+            pst.setLong(1, book.getISBN());
+            pst.setInt(2, book.getAuthor_id());
+            pst.setString(3, book.getTitle());
+            pst.setString(4, book.getPublisher_id());
+            pst.setInt(5, book.getPublication_year());
+            pst.setInt(6, book.getPrice());
+
+            pst.executeUpdate();
+
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    @Override
+    public void updateBook(Book book) {
+
+    }
+
+    @Override
+    public List<Book> getBooks() {
+        return null;
+    }
+
+    @Override
+    public void deleteBook(long ISBN) {
+
+    }
+
 }
