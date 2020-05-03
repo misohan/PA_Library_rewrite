24a6b9877da67ed0b8395a4c0e1f39de411e9e69
misohan <michal.hanusniak@aiesec.cz>
2020-05-03 14:31:24 +0200

    Add methods in interface AuthorDAO

--- src/main/java/codecooler/michal/com/DAO/AuthorDAO.java
+++ src/main/java/codecooler/michal/com/DAO/AuthorDAO.java
@@ -1,4 +1,15 @@
 package codecooler.michal.com.DAO;
 
+import codecooler.michal.com.model.Author;
+
+import java.util.List;
+
 public interface AuthorDAO {
+    void createAuthor(Author author);
+
+    void updateAuthor(Author author);
+
+    List<Author> getAuthors();
+
+    void deleteAuthor(int id);
 }
--- src/main/java/codecooler/michal/com/DAO/AuthorJDBCDAO.java
+++ src/main/java/codecooler/michal/com/DAO/AuthorJDBCDAO.java
@@ -1,4 +1,18 @@
 package codecooler.michal.com.DAO;
+import codecooler.michal.com.LibrarySQLConnection;
+import codecooler.michal.com.model.Book;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class AuthorJDBCDAO {
+    private final LibrarySQLConnection dbConn = new LibrarySQLConnection();
+    private LibrarySQLConnection connection;
+
+
+
 }
