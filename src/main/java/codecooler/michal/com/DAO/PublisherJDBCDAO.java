04d80b5da915e28f0764baa5f2eacfbb5baa0d83
misohan <michal.hanusniak@aiesec.cz>
2020-05-03 16:06:04 +0200

    Update PublisherJDBCDAO

--- src/main/java/codecooler/michal/com/DAO/PublisherJDBCDAO.java
+++ src/main/java/codecooler/michal/com/DAO/PublisherJDBCDAO.java
@@ -1,5 +1,46 @@
 package codecooler.michal.com.DAO;
 
-public class PublisherJDBCDAO {
+import codecooler.michal.com.LibrarySQLConnection;
+import codecooler.michal.com.model.Publisher;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.List;
+
+public class PublisherJDBCDAO implements PublisherDAO{
+    private final LibrarySQLConnection dbConn = new LibrarySQLConnection();
+
+    @Override
+    public void createPublisher(Publisher publisher) {
+        String sql = "INSERT INTO authors (ID, \"name\") " +
+                "VALUES (?,?)";
+
+        try (Connection con = dbConn.connect();
+             PreparedStatement pst = con.prepareStatement(sql)) {
+
+            pst.setString(1, publisher.getId());
+            pst.setString(2, publisher.getName());
+
+            pst.executeUpdate();
+
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    @Override
+    public void updatePublisher(Publisher publisher) {
+
+    }
+
+    @Override
+    public List<Publisher> getPublishers() {
+        return null;
+    }
+
+    @Override
+    public void deletePublisher(String id) {
+
+    }
 }
