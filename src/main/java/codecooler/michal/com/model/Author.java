41f10d3a99a1db3b9ac4f6c27739abd3ac12cb27
misohan <michal.hanusniak@aiesec.cz>
2020-05-03 14:24:31 +0200

    Add fields to Author class

--- src/main/java/codecooler/michal/com/model/Author.java
+++ src/main/java/codecooler/michal/com/model/Author.java
@@ -1,4 +1,37 @@
 package codecooler.michal.com.model;
 
 public class Author {
+    int id;
+    String firstName;
+    String secondName;
+
+    public Author(int id, String firstName, String secondName) {
+        this.id = id;
+        this.firstName = firstName;
+        this.secondName = secondName;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getSecondName() {
+        return secondName;
+    }
+
+    public void setSecondName(String secondName) {
+        this.secondName = secondName;
+    }
 }
