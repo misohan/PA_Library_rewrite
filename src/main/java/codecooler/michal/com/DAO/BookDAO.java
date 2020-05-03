9a2f38de92dd465e374eba40b5ef96aa10da37d8
misohan <michal.hanusniak@aiesec.cz>
2020-05-03 10:26:58 +0200

    Create methods

--- src/main/java/codecooler/michal/com/DAO/BookDAO.java
+++ src/main/java/codecooler/michal/com/DAO/BookDAO.java
@@ -1,4 +1,15 @@
 package codecooler.michal.com.DAO;
 
+import java.util.List;
+import codecooler.michal.com.model.Book;
+
+
 public interface BookDAO {
+    void createBook(Book book);
+
+    void updateBook(Book book);
+
+    List<Book> getBooks();
+
+    void deleteBook(long ISBN);
 }
