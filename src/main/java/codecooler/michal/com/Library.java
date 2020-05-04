5ac75a56eabe469a85cbd244593b28e29a9b4a0c
misohan <michal.hanusniak@aiesec.cz>
2020-05-04 23:42:16 +0200

    Update Library main class

--- src/main/java/codecooler/michal/com/Library.java
+++ src/main/java/codecooler/michal/com/Library.java
@@ -1,12 +1,14 @@
 package codecooler.michal.com;
 
+import codecooler.michal.com.DAO.BookDAO;
+import codecooler.michal.com.DAO.BookJDBCDAO;
+
 public class Library {
     public static void main(String[] args) {
+        BookDAO bookDAO = new BookJDBCDAO();
+        LibraryController libraryController = new LibraryController(bookDAO);
 
-       LibraryController libraryController = new LibraryController();
-       libraryController.libraryOptions();
-
-
+        libraryController.libraryOptions();
     }
 
 }
